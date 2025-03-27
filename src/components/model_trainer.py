6e441727d9db440ddad66009f71eea191cbd0251
tj003 <tusharj071@gmail.com>
2025-03-27 13:44:14 +0530

    Hyperparamter tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -6,9 +6,8 @@ AdaBoostRegressor,
 GradientBoostingRegressor,
 RandomForestRegressor,
 )
-from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 from src.exception import CustomException
@@ -37,16 +36,46 @@ class ModelTrainer:
 
             models = {
                     "Linear Regression": LinearRegression(),
-                    "Lasso": Lasso(),
-                    "Ridge": Ridge(),
-                    "K-Neighbors Regressor": KNeighborsRegressor(),
                     "Decision Tree": DecisionTreeRegressor(),
-                    "GradientBoostingRegressor": GradientBoostingRegressor(),
+                    "Gradient Boosting Regressor": GradientBoostingRegressor(),
                     "Random Forest Regressor": RandomForestRegressor(),
                     "XGBRegressor": XGBRegressor(), 
                     "AdaBoost Regressor": AdaBoostRegressor()
                     }
-            model_report:dict=evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest Regressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting Regressor":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+            model_report:dict=evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, param=params)
 
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
@@ -61,7 +90,7 @@ class ModelTrainer:
             if best_model_score< 0.6:
                 raise CustomException("No best model found")
             
-            logging.info(f"Best model on both training and testing dataset")
+            logging.info(f"Best model on both training and testing dataset saved")
             
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
